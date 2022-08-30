f1c776fb5e404fdcbd041561b8e4904eea7782d7
Anne McCorkle <annefmccorkle@gmail.com>
2022-08-30 13:34:22 -0600

    writing initial routes

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,32 @@
+const router = require('express').Router();
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,  
+  deleteFriend,
+} = require('../../controllers/userController');
+
+//routes
+// /api/users
+router.route('/')
+  .get(getUsers)
+  .post(createUser);
+
+//single user route
+// /api/users/:userId 
+router.route('/:userId')
+  .get(getSingleUser)
+  .put(updateUser)
+  .delete(deleteUser);
+
+//friend route
+// /api/users/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId')
+  .post(addFriend)
+  .delete(deleteFriend);
+
+
+module.exports = router;
